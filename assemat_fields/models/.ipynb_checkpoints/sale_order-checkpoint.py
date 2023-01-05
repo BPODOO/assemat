6408e9da628b9edf1dabe98444971cd038f56e3e
Your Name <you@example.com>
2023-01-05 16:13:37 +0000

    [ADD] Pre assignement des valeurs sur le devis + onchange sur chantier

--- assemat_fields/models/.ipynb_checkpoints/sale_order-checkpoint.py
+++ assemat_fields/models/.ipynb_checkpoints/sale_order-checkpoint.py
@@ -6,8 +6,9 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    bp_worksite = fields.Many2one('project.project', copy=False)
+    bp_worksite = fields.Many2one('project.project', string="Chantier", copy=False)
     
-    # @api.onchange('bp_worksite')
-    # def _onchange_worksite(self):
-    #     self.partner_id = self.bp_worksite.partner_id
+    @api.onchange('bp_worksite')
+    def _onchange_worksite(self):
+        self.partner_id = self.bp_worksite.partner_id
+        self.analytic_account_id = self.bp_worksite.analytic_account_id
