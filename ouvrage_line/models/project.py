0df9288155025910635f4edca27b16cd1b02747a
Your Name <you@example.com>
2023-03-02 15:04:05 +0000

    [BUGFIX] Reset n'affiche plus toutes les lignes

--- ouvrage_line/models/project.py
+++ ouvrage_line/models/project.py
@@ -7,14 +7,17 @@ class Project(models.Model):
     
     
     def action_open_material_lines_associated(self):
-        action = self.env["ir.actions.actions"]._for_xml_id("ouvrage_line.material_lines_action_open_associated")
+        view_id = self.env.ref('ouvrage_line.material_line_tree_bp').id
         order_ids = self.env['sale.order'].search([('bp_worksite','=',self.id),('state','=','sale')]).ids
-        action['context'] = {
-            'flags': {'hasSelectors': False},
-            'group_by': 'bp_sale_order_line_id',
-            'search_default_bp_sale_order_id':  [order_ids],
-        }
+        action = { 
+            'type': 'ir.actions.act_window', 
+            'name': 'Ligne(s) matériel(s)', 
+            'view_type': 'form',
+            'views': [[view_id, 'tree']],
+            'view_mode': 'tree,form',
+            'target': 'current',
+            'context': {'group_by': ['bp_sale_order_line_id']},
+            'res_model': 'material.line',
+            'domain': [('bp_sale_order_id.id','in', order_ids)]
+          }
         return action
-
-
-    
