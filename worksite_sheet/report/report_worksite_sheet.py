f240c4da2bd03f2c6d493039a9d7564375d09ef6
Your Name <you@example.com>
2023-03-16 13:39:25 +0000

    [UPDATE] Ajout temps prévue sur la fiche chantier

--- worksite_sheet/report/report_worksite_sheet.py
+++ worksite_sheet/report/report_worksite_sheet.py
@@ -10,8 +10,9 @@ class ReportWorksiteSheet(models.AbstractModel):
     #Déclenchement lors de l'impression
     def _get_report_values(self, docids, data=None):
         
+        fabrication_lines = self.env['fabrication'].search([['bp_sale_order_line_id', 'in', data['data']['sale_line_ids']]])
         materials_lines = self.env['material.line'].search([['bp_sale_order_line_id', 'in', data['data']['sale_line_ids']]])
-        materials_lines_group = self.group_materials_by_sale_order_line(materials_lines)
+        materials_lines_group = self.group_materials_by_sale_order_line(materials_lines,fabrication_lines)
         return {
             'doc_ids' : docids,
             # 'docs': docs,
@@ -21,15 +22,28 @@ class ReportWorksiteSheet(models.AbstractModel):
             'project_id': self.env['project.project'].browse(data['data']['project_id']),
         }
     
-    def group_materials_by_sale_order_line(self,records):
+    def group_materials_by_sale_order_line(self,records,fabrications):
         materials_group = {}
+        fabrication_totals = self._time_total_by_sale_order_line(fabrications)
         for record in records:
             key = record.bp_sale_order_line_id.name
-            material_line = {'qty_total': 0, 'lines': []}
+            material_line = {'qty_total': 0, 'lines': [], 'time_total': 0}
             if key in materials_group.keys():
                 materials_group[key]['qty_total'] += record.bp_qty
                 materials_group[key]['lines'].append(record)
             else:
                 materials_group[key] = {'qty_total': record.bp_qty, 'lines': [record]}
+        for fab_key in fabrication_totals:
+            materials_group[fab_key]['time_total'] = fabrication_totals[fab_key]['time_total']
         return materials_group
             
+    def _time_total_by_sale_order_line(self,records):
+        fabrication_group = {}
+        for record in records:
+            key = record.bp_sale_order_line_id.name
+            fabrication_line = {'time_total': 0}
+            if key in fabrication_group.keys():
+                fabrication_group[key]['time_total'] += record.bp_duration
+            else:
+                fabrication_group[key] = {'time_total': record.bp_duration}
+        return fabrication_group
