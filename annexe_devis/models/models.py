82913c6c2f6608eb0e11108d9ae7319417cac046
Your Name <you@example.com>
2023-03-29 07:08:38 +0000

    [UPDATE] comment img2pdf

--- annexe_devis/models/models.py
+++ annexe_devis/models/models.py
@@ -81,7 +81,6 @@ class SaleOrder(models.Model):
         attachment_ids = self.bp_upload_annexes
 
         writer = PdfFileWriter()
-
         #Création d'un seul tableau avec les PDF du produit et les PDF des OT, en BytesIO
         pdf_files = [{'filename':attachment_id.name,'data': io.BytesIO(base64.b64decode(attachment_id.datas))} for attachment_id in attachment_ids]
         pdf_files.insert(0, {'filename': False,'data': io.BytesIO(report_output[0])})
@@ -92,8 +91,11 @@ class SaleOrder(models.Model):
                 type_file = mimetypes.guess_type(pdf['filename'])
                 name_type = type_file[0].split('/')
                 if name_type[0] == 'image':
+                    #page size format A4
                     a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+                    # Mode shrink pour concerver la taille des images
                     layout_fun = img2pdf.get_layout_fun(a4inpt, fit=img2pdf.FitMode.shrink ,auto_orient=True)
+                    # Convert image to PDF
                     document = img2pdf.convert(pdf['data'],layout_fun=layout_fun)
                     reader = PdfFileReader(io.BytesIO(document), strict=False)
                 else:
