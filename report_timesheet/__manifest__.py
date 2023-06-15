f9fdc50eb0b0ee005c3da9a7c60675da7386ed88
Your Name <you@example.com>
2023-06-15 12:05:34 +0000

    [INIT] Rapport feuille de temps

---
+++ report_timesheet/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "report_timesheet",
+
+    'summary': """
+        Modification du rapport de feuille de temps""",
+
+    'description': """
+        Ajout d'un regroupement par tâche puis par type de travaux
+    """,
+
+    'author': "BeProject",
+    'website': "https://beproject.fr/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
