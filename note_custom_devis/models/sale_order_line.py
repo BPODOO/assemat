d869a4d81330df9593380819b0327e0bd22e53e9
Your Name <you@example.com>
2023-10-03 14:52:07 +0000

    [UPDATE] note personalisée affichage rapport

--- note_custom_devis/models/sale_order_line.py
+++ note_custom_devis/models/sale_order_line.py
@@ -13,11 +13,17 @@ class SaleOrderLine(models.Model):
        
     name_without_html = fields.Html(string='Description bis')
 
+    # @api.onchange('name')
+    # def load_name_desc_devis(self):
+    #     if self.name:
+    #         html = "<p>" + self.name.replace("\n", "<br>") + "</p>"
+    #         self.name_without_html = html
+    #         _logger.info(self.name_without_html)
+
     @api.onchange('name')
     def load_name_desc_devis(self):
-        if self.name:
-            html = "<p>" + self.name.replace("\n", "<br>") + "</p>"
-            self.name_without_html = html
+        _logger.info(self.name_without_html)
+        return
     
     def action_show_details(self):
         view = self.env.ref('note_custom_devis.view_sale_order_line_desc_details_form_bp')
@@ -39,10 +45,13 @@ class SaleOrderLine(models.Model):
         return regex.sub('', string)
     
     def action_save(self):
+        _logger.info(self.name_without_html)
         note_custom = self.name_without_html
         if 'img' in note_custom:
-            src_img = note_custom[note_custom.find('/')+len('/'):note_custom.find('?')].split('/').pop()
+            src_img = note_custom[note_custom.find('/')+len('/'):note_custom.find('?')]
+            #.split('/').pop()
             self.name = src_img
         else:
             self.name = self.remove_html(self.name_without_html)
         return {'type': 'ir.actions.act_window_close'}
+        
